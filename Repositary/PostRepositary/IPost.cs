8c1bceb0c0a2d94018b4c38ccd067f929827f910
kemish bajgai <kemishbajgai4@gmail.com>
2026-01-07 19:28:16 +0545

    feat:Post controller and repositary add

---
+++ Repositary/PostRepositary/IPost.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+using BlogWebsite.Models;
+
+namespace BlogWebsite.Repositary.PostRepositary
+{
+    public interface IPost
+    {
+        Task<IEnumerable<Post>> GetAllPosts();
+        Task<Post?> GetPost(Expression<Func<Post, bool>>? filter);
+        Task AddPost(Post post);
+        Task UpdatePost(Post post);
+        Task DeletePost(Post post);
+    }
+}
---
+++ Repositary/PostRepositary/PostRepo.cs
@@ -0,0 +1,48 @@
+﻿using BlogWebsite.Database;
+using BlogWebsite.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace BlogWebsite.Repositary.PostRepositary
+{
+    public class PostRepo : IPost
+    {
+        private readonly AppDbContext _context;
+
+        public PostRepo(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+        public async Task AddPost(Post post)
+        {
+            _context.Posts.Add(post);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeletePost(Post post)
+        {
+            _context.Posts.Remove(post);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Post>> GetAllPosts()
+        {
+            return await _context.Posts
+                    .Include(p => p.Comments)
+                    .Include(p => p.Category)
+                    .ToListAsync();
+        }
+
+        public async Task<Post?> GetPost(Expression<Func<Post, bool>>? filter)
+        {
+
+            return await _context.Posts.Where(filter).FirstOrDefaultAsync();
+        }
+
+        public async Task UpdatePost(Post post)
+        {
+            _context.Update(post);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
